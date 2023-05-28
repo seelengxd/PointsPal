5ea4f5cdf02e4692dbdd46eddc91666a7ee5f472
astraxq <brianquek@hotmail.com>
2023-05-28 14:54:47 +0800

    test page for testing

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,15 +1,15 @@
-import {createBrowserRouter} from 'react-router-dom';
-import App from '../App';
+import { createBrowserRouter } from 'react-router-dom';
+import Test from '../pages/Test/Test';
 
 const Router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-    },
-    {
-      path: "/about",
-      element: <h1>About</h1>,
-    }
-  ]);
+  {
+    path: '/',
+    element: <Test />,
+  },
+  {
+    path: '/about',
+    element: <h1>About</h1>,
+  },
+]);
 
 export default Router;
