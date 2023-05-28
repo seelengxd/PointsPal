5d1ea454d21102169af1e173b9a860a96fc2df14
astraxq <brianquek@hotmail.com>
2023-05-28 15:29:22 +0800

    array rendering

--- frontend/src/component/commons/Banner.tsx
+++ frontend/src/component/commons/Banner.tsx
@@ -5,14 +5,7 @@ type Props = {
 // Image Credits: Photo by <a href="https://unsplash.com/pt-br/@lvnatikk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lily Banse</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
 
 const Banner = ({ imageUrl }: Props) => {
-  return (
-    <img
-      src={
-        'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
-      }
-      className='w-full'
-    ></img>
-  );
+  return <img src={imageUrl} className='w-full'></img>;
 };
 
 export default Banner;
--- frontend/src/pages/Home/Merchant/Merchant.tsx
+++ frontend/src/pages/Home/Merchant/Merchant.tsx
@@ -1,10 +1,17 @@
 import Banner from '../../../component/commons/Banner';
 
 const Merchant = () => {
+  const imgLinks: string[] = [
+    'https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    'https://plus.unsplash.com/premium_photo-1663852297801-d277b7af6594?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+  ];
+
   return (
     <>
       <h1>Merchant</h1>
-      <Banner />
+      {imgLinks.map((link, key) => {
+        return <Banner imageUrl={link} key={key} />;
+      })}
     </>
   );
 };
