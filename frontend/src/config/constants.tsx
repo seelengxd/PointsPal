fbb770b39e96fa9d82518bf06fbe9f49e321402a
see leng <seelengxd@gmail.com>
2023-05-30 08:24:14 +0800

    🤡: small frontend stuff
    - make logo fit
    - change constants because somehow we have that and a .env

--- frontend/src/config/constants.tsx
+++ frontend/src/config/constants.tsx
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:5001";
+export const BACKEND_URL = 'https://ec2-3-106-139-115.ap-southeast-2.compute.amazonaws.com:5001';
--- frontend/src/pages/Authentication/Login.tsx
+++ frontend/src/pages/Authentication/Login.tsx
@@ -1,51 +1,46 @@
-import {
-    Box,
-    Button,
-    Stack,
-    CircularProgress,
-  } from '@mui/material'
-import { useCallback, useState } from 'react'
-import sgidLogo from '../../assets/logo.png'
-import singpassLogo from '../../assets/singpass.svg'
-import { useAuth } from '../../hooks/useAuth'
-import { Navigate } from 'react-router-dom'
-import { BACKEND_URL } from '../../config/constants'
-  
+import { Box, Button, Stack, CircularProgress } from '@mui/material';
+import { useCallback, useState } from 'react';
+import sgidLogo from '../../assets/logo.png';
+import singpassLogo from '../../assets/singpass.svg';
+import { useAuth } from '../../hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+import { BACKEND_URL } from '../../config/constants';
+
 export const Login = (): JSX.Element => {
   // Button loading state
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLoginBtnClick = useCallback(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     fetch(`${BACKEND_URL}/api/auth-url`, {
       credentials: 'include',
     })
-      .then(async (r) => await r.json())
+      .then(async r => await r.json())
       .then(({ url }) => {
-        window.location.href = url
+        window.location.href = url;
       })
       .catch((e: unknown) => {
         setIsLoading(false);
         console.log(e);
-      })
-  }, [])
+      });
+  }, []);
 
-  const { user, isLoading: isUserLoading } = useAuth()
+  const { user, isLoading: isUserLoading } = useAuth();
 
   if (isUserLoading) {
-    return <CircularProgress />
+    return <CircularProgress />;
   }
   if (user !== null) {
-    return <Navigate to="/logged_in" />
+    return <Navigate to='/logged_in' />;
   }
   return (
-    <Stack spacing="48px" direction="column">
-      <Stack spacing="48px" justifyContent={'center'} direction="row">
+    <Stack spacing='48px' direction='column'>
+      <Stack spacing='48px' justifyContent={'center'} direction='row'>
         <Box>
-          <img src={sgidLogo} width="100%" />
+          <img src={sgidLogo} width='100%' />
         </Box>
         <Box>
-          <img src={singpassLogo} width="100%" />
+          <img src={singpassLogo} width='100%' className='' />
         </Box>
       </Stack>
       {/* <FormControl id={iceCreamFieldId} mb={6}>
@@ -69,9 +64,7 @@ export const Login = (): JSX.Element => {
           </Stack>
         </Radio.RadioGroup>
       </FormControl> */}
-      <Button onClick={handleLoginBtnClick}>
-        Login with Singpass app
-      </Button>
+      <Button onClick={handleLoginBtnClick}>Login with Singpass app</Button>
     </Stack>
-  )
-}
+  );
+};
